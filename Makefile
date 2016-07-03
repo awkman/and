0af2aed0a398a1f54cf910d912ad440086835bd2
Awk Jiang <shengjhih@gmail.com>
2016-07-04 00:28:35 +0800

    init submit

---
+++ Makefile
@@ -0,0 +1,10 @@
+CC=gcc
+CFLAGS=
+
+obj-m += and.o
+
+all:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
+clean:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
---
+++ and.c
@@ -0,0 +1,21 @@
+
+#include <linux/module.h>
+
+MODULE_AUTHOR("Jiang Sheng-Jhih");
+MODULE_DESCRIPTION("Awk Network Driver");
+MODULE_LICENSE("LGPL");
+
+
+static int __init and_init(void)
+{
+	printk(KERN_ERR "%s\n", __func__);
+	return 0;
+}
+
+static void __exit and_exit(void)
+{
+	printk(KERN_ERR "%s\n", __func__);
+}
+
+module_init(and_init);
+module_exit(and_exit);
